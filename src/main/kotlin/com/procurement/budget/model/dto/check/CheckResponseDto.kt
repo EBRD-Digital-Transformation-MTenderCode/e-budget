1d8725712d0bb6a56b3140a19c35fba0fad546c9
andreykolchev <andreykolchev@gmail.com>
2018-06-27 13:50:27 +0300

    json validation

--- src/main/kotlin/com/procurement/budget/model/dto/check/CheckResponseDto.kt
+++ src/main/kotlin/com/procurement/budget/model/dto/check/CheckResponseDto.kt
@@ -1,18 +1,14 @@
 package com.procurement.budget.model.dto.check
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.procurement.budget.model.dto.ei.EiOrganizationReferenceDto
 import com.procurement.budget.model.dto.fs.FsOrganizationReferenceDto
 import java.util.*
-import javax.validation.Valid
-import javax.validation.constraints.NotEmpty
 
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
 data class CheckResponseDto(
 
-        val ei: HashSet<String>?,
+        val ei: Set<String>?,
 
         val budgetBreakdown: List<CheckBudgetBreakdownDto>,
 
