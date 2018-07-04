c9d5acb532d0dcb0c9bd003d532f9be89e96712f
andreykolchev <andreykolchev@gmail.com>
2018-07-04 15:53:33 +0300

    json validation

--- src/main/kotlin/com/procurement/budget/model/dto/databinding/MoneyDeserializer.kt
+++ src/main/kotlin/com/procurement/budget/model/dto/databinding/MoneyDeserializer.kt
@@ -1,6 +1,7 @@
 package com.procurement.budget.model.dto.databinding
 
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonToken
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.deser.std.NumberDeserializers
@@ -16,6 +17,9 @@ class MoneyDeserializer : JsonDeserializer<BigDecimal>() {
 
     @Throws(IOException::class)
     override fun deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): BigDecimal {
+        if (jsonParser.currentToken == JsonToken.VALUE_STRING) {
+            throw ErrorException(ErrorType.INVALID_JSON_TYPE, jsonParser.currentName)
+        }
         var bd = delegate.deserialize(jsonParser, deserializationContext)
         if (bd < BigDecimal.valueOf(0.00)) throw ErrorException(ErrorType.INVALID_JSON_TYPE, jsonParser.currentName)
         bd = bd.setScale(2, RoundingMode.HALF_UP)
