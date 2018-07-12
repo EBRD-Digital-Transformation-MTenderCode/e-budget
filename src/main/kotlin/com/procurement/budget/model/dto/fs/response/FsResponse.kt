5516a17ce92fb8a96df12a86c108985192fbb057
andreykolchev <andreykolchev@gmail.com>
2018-07-12 22:31:53 +0300

    fs

--- src/main/kotlin/com/procurement/budget/model/dto/fs/response/FsResponse.kt
+++ src/main/kotlin/com/procurement/budget/model/dto/fs/response/FsResponse.kt
@@ -1,6 +1,7 @@
 package com.procurement.budget.model.dto.fs.response
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.procurement.budget.model.dto.ei.ValueEi
 import com.procurement.budget.model.dto.fs.Fs
 
 data class FsResponse @JsonCreator constructor(
@@ -8,4 +9,19 @@ data class FsResponse @JsonCreator constructor(
         val ei: EiForFs?,
 
         val fs: Fs
+)
+
+data class EiForFs @JsonCreator constructor(
+
+        var planning: EiForFsPlanning
+)
+
+data class EiForFsPlanning @JsonCreator constructor(
+
+        var budget: EiForFsBudget
+)
+
+data class EiForFsBudget @JsonCreator constructor(
+
+        var amount: ValueEi?
 )
