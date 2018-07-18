04d121bec7948e7fa26e3cff3381425a37d24549
andreykolchev <andreykolchev@gmail.com>
2018-07-18 16:10:20 +0300

    updateEi

--- src/main/kotlin/com/procurement/budget/model/dto/ocds/Address.kt
+++ src/main/kotlin/com/procurement/budget/model/dto/ocds/Address.kt
@@ -2,6 +2,7 @@ package com.procurement.budget.model.dto.ocds
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
+import javax.validation.Valid
 import javax.validation.constraints.NotNull
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,12 +13,60 @@ data class Address @JsonCreator constructor(
 
         val postalCode: String?,
 
-        @field:NotNull
-        val locality: String,
+        @field:Valid @field:NotNull
+        val addressDetails: AddressDetails
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class AddressDetails(
+
+        @field:Valid @field:NotNull
+        val country: Country,
+
+        @field:Valid @field:NotNull
+        val region: Region,
+
+        @field:Valid @field:NotNull
+        val locality: Locality
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class Country(
+
+        val scheme: String?,
 
         @field:NotNull
-        val region: String,
+        val id: String,
+
+        val description: String?,
+
+        val uri: String?
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class Region(
+
+        val scheme: String?,
 
         @field:NotNull
-        val countryName: String
+        val id: String,
+
+        val description: String?,
+
+        val uri: String?
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class Locality(
+
+        @field:NotNull
+        val scheme: String,
+
+        @field:NotNull
+        val id: String,
+
+        @field:NotNull
+        val description: String,
+
+        val uri: String?
 )
