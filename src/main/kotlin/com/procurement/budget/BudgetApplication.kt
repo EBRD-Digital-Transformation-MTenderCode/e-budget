69e63a57b1e9fc459380a7e62ff10e0dd167743c
andreykolchev <andreykolchev@gmail.com>
2018-09-21 14:37:13 +0300

    swarm

--- src/main/kotlin/com/procurement/budget/BudgetApplication.kt
+++ src/main/kotlin/com/procurement/budget/BudgetApplication.kt
@@ -3,10 +3,8 @@ package com.procurement.budget
 import com.procurement.budget.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
-@EnableEurekaClient
 class BudgetApplication
 
 fun main(args: Array<String>) {
