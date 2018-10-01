665e6f4703f383368f1c73dcb526468c76e50620
andreykolchev <andreykolchev@gmail.com>
2018-10-01 16:38:09 +0300

    ES-1031

--- src/main/kotlin/com/procurement/budget/model/dto/ocds/ContactPoint.kt
+++ src/main/kotlin/com/procurement/budget/model/dto/ocds/ContactPoint.kt
@@ -7,18 +7,14 @@ import javax.validation.constraints.NotNull
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class ContactPoint @JsonCreator constructor(
 
-        @field:NotNull
         val name: String,
 
-        @field:NotNull
         val email: String,
 
-        @field:NotNull
         val telephone: String,
 
         val faxNumber: String?,
 
-        @field:NotNull
         val url: String?
 )
 
