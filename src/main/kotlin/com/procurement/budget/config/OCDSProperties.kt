304b5813e51f5bb2ce83aca8c5bcadd34221e780
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-10-21 14:03:45 +0300

    ES-2021 Removed 'Component' annotation from 'OCDSProperties'.

--- src/main/kotlin/com/procurement/budget/config/OCDSProperties.kt
+++ src/main/kotlin/com/procurement/budget/config/OCDSProperties.kt
@@ -1,9 +1,7 @@
 package com.procurement.budget.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
 
-@Component
 @ConfigurationProperties(prefix = "ocds")
 class OCDSProperties(
     var prefixes: Prefixes? = null
