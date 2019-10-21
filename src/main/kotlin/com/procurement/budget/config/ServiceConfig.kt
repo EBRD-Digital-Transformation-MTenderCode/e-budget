331d24a69c21e2110fb8c76b1c3585d567826772
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-10-21 13:44:47 +0300

    ES-2021 Enabled loading properties of the OCDS.

--- src/main/kotlin/com/procurement/budget/config/ServiceConfig.kt
+++ src/main/kotlin/com/procurement/budget/config/ServiceConfig.kt
@@ -1,8 +1,10 @@
 package com.procurement.budget.config
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ComponentScan(basePackages = ["com.procurement.budget.service"])
+@EnableConfigurationProperties(value = [OCDSProperties::class])
 class ServiceConfig
