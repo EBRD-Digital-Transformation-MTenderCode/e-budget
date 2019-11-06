fb94f8c616dbdbf979efc21e5fec5f416b4ba674
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-06 10:28:14 +0200

    ES-2146 Added logging to controller.

--- src/main/kotlin/com/procurement/budget/controller/CommandController.kt
+++ src/main/kotlin/com/procurement/budget/controller/CommandController.kt
@@ -2,17 +2,33 @@ package com.procurement.budget.controller
 
 import com.procurement.budget.exception.EnumException
 import com.procurement.budget.exception.ErrorException
-import com.procurement.budget.model.dto.bpe.*
+import com.procurement.budget.model.dto.bpe.CommandMessage
+import com.procurement.budget.model.dto.bpe.ResponseDto
+import com.procurement.budget.model.dto.bpe.getEnumExceptionResponseDto
+import com.procurement.budget.model.dto.bpe.getErrorExceptionResponseDto
+import com.procurement.budget.model.dto.bpe.getExceptionResponseDto
 import com.procurement.budget.service.CommandService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @Validated
 @RestController
 @RequestMapping("/command")
 class CommandController(private val commandService: CommandService) {
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(CommandController::class.java)
+    }
 
     @PostMapping
     fun command(@RequestBody commandMessage: CommandMessage): ResponseEntity<ResponseDto> {
@@ -23,6 +39,7 @@ class CommandController(private val commandService: CommandService) {
     @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(Exception::class)
     fun exception(ex: Exception): ResponseDto {
+        log.error("Error.", ex)
         return when (ex) {
             is ErrorException -> getErrorExceptionResponseDto(ex)
             is EnumException -> getEnumExceptionResponseDto(ex)
