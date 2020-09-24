045a6b835c895cd9c398d6b39b4219c53a63dacb
katherine <kate.shaidurova@gmail.com>
2020-09-24 13:05:19 +0300

    ES-4318 Added CollectionExtension.kt

---
+++ src/main/kotlin/com/procurement/budget/utils/CollectionExtension.kt
@@ -0,0 +1,44 @@
+package com.procurement.budget.utils
+
+inline fun <T, V> Collection<T>.isUnique(selector: (T) -> V): Boolean {
+    val unique = HashSet<V>()
+    forEach { item ->
+        if (!unique.add(selector(item))) return false
+    }
+    return true
+}
+
+inline fun <T, V> Collection<T>.getDuplicate(selector: (T) -> V): T? {
+    val unique = HashSet<V>()
+    forEach { item ->
+        if (!unique.add(selector(item))) return item
+    }
+    return null
+}
+
+inline fun <T, V> Collection<T>.toSetBy(selector: (T) -> V): Set<V> {
+    val collections = LinkedHashSet<V>()
+    forEach {
+        collections.add(selector(it))
+    }
+    return collections
+}
+
+inline fun <reified T> T.asList() = listOf(this)
+
+fun <T> getMissingElements(received: Iterable<T>, known: Iterable<T>): Set<T> =
+    known.asSet().subtract(received.asSet())
+
+fun <T> getUnknownElements(received: Iterable<T>, known: Iterable<T>) =
+    getNewElements(received = received, known = known)
+
+fun <T> getNewElements(received: Iterable<T>, known: Iterable<T>): Set<T> =
+    received.asSet().subtract(known.asSet())
+
+fun <T> getElementsForUpdate(received: Iterable<T>, known: Iterable<T>): Set<T> =
+    known.asSet().intersect(received.asSet())
+
+private fun <T> Iterable<T>.asSet(): Set<T> = when (this) {
+    is Set -> this
+    else -> this.toSet()
+}
